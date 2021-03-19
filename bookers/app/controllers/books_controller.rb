1604c7db437c86df4ec2091b269f81fac7b0d650
otinu <fitshter@gmail.com>
2021-03-19 02:55:40 +0000

    save

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -4,8 +4,34 @@ class BooksController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def edit
   end
+
+  def new
+    @book = Book.new
+  end
+
+# まだ未完成～～～～～～～～～～～～～～～～
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to show_books_path(blog.id)
+
+  end
+
+
+
+  private
+  def book_params
+    params.require(:book).permit(:本のタイトル, :感想)
+  end
+
+
+
+
+
+
 end
